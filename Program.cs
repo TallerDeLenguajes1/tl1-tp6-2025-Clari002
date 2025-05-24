a9366eddf3c7ddbb3798ac69be6d3a22778ac1fc
Claribel <claridiaz02@gmail.com>
2025-05-23 22:56:19 -0300

    se agrega ejercicio 2 en CalculadoraV1

--- Program.cs
+++ Program.cs
@@ -27,3 +27,77 @@ else
     }
     Console.WriteLine("Valor invertido: " + nInvertido);
 }
+
+//Ejercicio 2
+Console.WriteLine("--------------------CALCULADORA------------------");
+int n1, n2;
+Console.WriteLine("Ingrese primer numero: ");
+if (!int.TryParse(Console.ReadLine(), out n1))
+{
+    Console.WriteLine("El valor que ha ingresado no es un numero");
+    return;
+}
+else
+{
+    Console.WriteLine("Ingrese segundo numero: ");
+    if (!int.TryParse(Console.ReadLine(), out n2))
+    {
+        Console.WriteLine("El valor que ha ingresado no es un numero");
+        return;
+    }
+    else
+    {
+        int valor;
+        do
+        {
+            Console.WriteLine("-------¿Que deseas realizar?------");
+            Console.WriteLine("(1)Sumar");
+            Console.WriteLine("(2)Restar");
+            Console.WriteLine("(3)Multiplicar");
+            Console.WriteLine("(4)Dividir");
+            Console.WriteLine("(0)Salir");
+            Console.WriteLine("Ingresar opcion: ");
+            if (!int.TryParse(Console.ReadLine(), out valor))
+            {
+                Console.WriteLine("El valor que ha ingresado no es un numero");
+                return;
+            }
+            else
+            {
+                switch (valor)
+                {
+                    case 1:
+                        int suma = n1 + n2;
+                        string cadenaSuma = suma.ToString();
+                        Console.WriteLine("La suma de " + n1 + " y de " + n2 + " es igual a: " + cadenaSuma);
+                        break;
+                    case 2:
+                        int resta;
+                        resta = n1 - n2;
+                        string cadenaResta = resta.ToString();
+                        Console.WriteLine("La resta de " + n1 + " y de " + n2 + " es igual a: " + cadenaResta);
+                        break;
+                    case 3:
+                        int producto;
+                        producto = n1 * n2;
+                        string cadenaProd = producto.ToString();
+                        Console.WriteLine("El producto de " + n1 + " y de " + n2 + " es igual a: " + cadenaProd);
+                        break;
+                    case 4:
+                        float division, aux1, aux2;
+                        aux1 = n1;
+                        aux2 = n2;
+                        if (n2 != 0)
+                        {
+                            division = aux1 / aux2;
+                            string cadenaDiv = division.ToString();
+                            Console.WriteLine("La division de " + n1 + " y de " + n2 + " es igual a: " + cadenaDiv);
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+        } while (valor != 0);
+    }
+}
